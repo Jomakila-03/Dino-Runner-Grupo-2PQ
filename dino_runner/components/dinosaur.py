1d2a47aee10a3a3def7063fbc6c6daa922f65fcd
Jomakila-03 <Paula.Quinchanegua@fundacion-jala.org>
2022-12-13 22:43:21 -0500

    Class: 2

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -14,13 +14,15 @@ class Dinosaur(Sprite):
  
     def __init__(self):
         self.image = RUNNING[0]
-        self.rect = self.image.get_rect()
-        self.rect.x = self.X_POS
-        self.rect.y = self.Y_POS
+        self.reset_rect()
         self.jump_velocity = self.JUMP_VELOCITY
         self.step = 0
-
         self.action = RUNNING_ACTION
+    
+    def reset_rect(self, y_pos=None):
+        self.rect = self.image.get_rect()
+        self.rect.x = self.X_POS
+        self.rect.y = y_pos or self.Y_POS
 
     def update(self, user_input):
         if self.action == RUNNING_ACTION:
@@ -42,26 +44,23 @@ class Dinosaur(Sprite):
             self.step = 0
 
     def run(self):
-        self.image = RUNNING[0] if self.step < 5 else RUNNING[1]
-        self.rect = self.image.get_rect()
-        self.rect.x = self.X_POS
-        self.rect.y = self.Y_POS
+        self.image = RUNNING[self.step // 5]
+        self.reset_rect()
         self.step += 1
     
     def jump(self):
         self.image = JUMPING
-        self.rect.y -= self.jump_velocity * 4
+        y_pos = self.rect.y - self.jump_velocity * 4
+        self.reset_rect(y_pos=y_pos)
         self.jump_velocity -= 0.8
         if self.jump_velocity < -self.JUMP_VELOCITY:
-            self.rect.y = self.Y_POS
+            self.reset_rect()
             self.jump_velocity = self.JUMP_VELOCITY
             self.action = RUNNING_ACTION
 
     def duck(self):
-        self.image = DUCKING[0] if self.step < 5 else DUCKING[1]
-        self.rect = self.image.get_rect()
-        self.rect.x = self.X_POS
-        self.rect.y = self.Y_POS_DUCK
+        self.image = DUCKING[self.step // 5] 
+        self.reset_rect(y_pos=self.Y_POS_DUCK)
         self.step += 1
         
     def draw(self,screen):
